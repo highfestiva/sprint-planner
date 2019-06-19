6a02f1f742e7db788c3b8ce64fecb8ccc2836d51
Jonas Bystrom <jonas.bystrom.2@consultant.volvo.com>
2019-06-19 13:38:11 +0200

    Add support for marking/unmarking days as "free"

--- static/mouse_mark.js
+++ static/mouse_mark.js
@@ -22,4 +22,13 @@ $(function () {
     .mouseup(function () {
       isMouseDown = false;
     });
+
+  // click date column
+  $("#ppl_dates_table th.col")
+    .mousedown(function () {
+      var index = $(this).index() + 1;
+      $(this).toggleClass("free");
+      var setFree = $(this).hasClass("free");
+      $("#ppl_dates_table td:nth-child("+index+")").toggleClass("free", setFree);
+    });
 });
