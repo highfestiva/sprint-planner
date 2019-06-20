3bc53c4e4bd9d1416b0972f567a9fba56e7f56aa
Jonas Bystrom <jonas.bystrom.2@consultant.volvo.com>
2019-06-20 13:11:51 +0200

    Add support for handling sprint lengths

--- static/mouse_mark.js
+++ static/mouse_mark.js
@@ -1,3 +1,8 @@
+function calcManDays(startIndex, endIndex) {
+  return [0, 2, 12];
+}
+
+
 $(function () {
   var isMouseDown = false,
       isFree = false,
@@ -30,5 +35,35 @@ $(function () {
       var index = $(this).index() + 1;
       $("th:nth-child("+index+")").toggleClass("free", setFree);
       $("td:nth-child("+index+")").toggleClass("free", setFree);
+    });
+
+  // check sprint start
+  $(".sprint_start_dates_table input[type=checkbox]").change(function() {
+      var starts = [0];
+      $(".sprint_start_dates_table input[type=checkbox]").each(function () {
+          if (this.checked) {
+            var dayIndex = $(this).parent().index() - 1;
+            if (dayIndex > 0) {
+              starts.push(dayIndex);
+            }
+          }
+        });
+      var html = "";
+      var prevIndex = 0;
+      for (var j = 0; j < starts.length; ++j) {
+        dayIndex = starts[j];
+        if (dayIndex <= 0) {
+          continue;
+        }
+        html += '<div class="availability"><h3>Sprint ' + j + '</h3>';
+        teamManDays = calcManDays(prevIndex, dayIndex);
+        for (var i = 0; i < teamManDays.length; ++i) {
+          var manDays = teamManDays[i];
+          html += "<p>Team " + (i+1) + ": " + manDays + " man-days</p>"
+        }
+        html += "</div>";
+        prevIndex = dayIndex;
+      }
+      $(".calc").html(html);
     });
 });
